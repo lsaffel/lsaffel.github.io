6d47ec563c70ca389180a3a02d94d9590d804ec6
lsaffel <58794458+lsaffel@users.noreply.github.com>
2020-09-14 21:13:59 -0600

    Add files via upload

---
+++ dogscriptgit.js
@@ -0,0 +1,18 @@
+// JavaScript source code
+
+/*	Author: Lisa Saffel
+			Date: June 1, 2020
+			Languages: HTML / CSS / Javascript
+			Purpose: A web site with an index, contact page and register page
+*/
+
+
+// function to check to be sure that the user wants to submit the form for sure or not - register page
+function confirm_submit() {
+    return confirm("Are you sure you want to submit?");
+}
+
+// function to check to be sure that the user wants to reset the form for sure or not - register page
+function confirm_reset() {
+    return confirm("Are you sure you want to reset?");
+}
